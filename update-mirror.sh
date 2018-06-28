640df616ad11ed3df000f3858a1536166e780bbc
Martin Monperrus <martin.monperrus@gnieh.org>
2018-06-28 11:12:19 +0200

    fix log output

--- update-mirror.sh
+++ update-mirror.sh
@@ -35,36 +35,36 @@ do
 	cd $ABSOLUTE_PATH_OF_LOCAL_REPOS
 	u=`echo $rep | cut -d '/' -f1`
 	r=`echo $rep | cut -d '/' -f2`
-	echo "Repo: $rep" >> $LOG_FILE
+	echo "Repo: $rep"
 
 	mirrors=`cat $MIRRORS_LIST | jq -r ".[] | select(.src==\"$rep\") | \"\(.mirrors)\""`
-	echo " -> mirrors: $mirrors" >> $LOG_FILE
+	echo " -> mirrors: $mirrors"
 
 	#If local copy does not exists
 	if [ ! -d "$r" ]; then
-		echo "$ABSOLUTE_PATH_OF_LOCAL_REPOS/$r does not exist." >> $LOG_FILE
-		git clone git@github.com:$u/$r.git >> $LOG_FILE 2>&1
+		echo "$ABSOLUTE_PATH_OF_LOCAL_REPOS/$r does not exist."
+		git clone git@github.com:$u/$r.git 2>&1
 	fi
 	cd $r
 
-	git pull >> $LOG_FILE 2>&1
+	git pull 2>&1 
 
 	#For all mirror
 	for m in `echo $mirrors | jq -r '.[]'`
 	do
-		echo "    mirror: $m" >> $LOG_FILE
+		echo "    mirror: $m"
 		remote_u=`echo $m | cut -d '/' -f1`
 		remote_r=`echo $m | cut -d '/' -f2`
 		
 		#if mirror is new
-		git remote get-url $remote_u >> $LOG_FILE 2>&1
+		git remote get-url $remote_u 2>&1
 		if [ $? -ne 0 ]; then
-			echo "Mirror $remote_u not registered yet" >> $LOG_FILE
-			git remote add $remote_u git@github.com:$remote_u/$remote_r.git >> $LOG_FILE 2>&1
+			echo "Mirror $remote_u not registered yet"
+			git remote add $remote_u git@github.com:$remote_u/$remote_r.git 2>&1
 		fi
 
 		#push to mirror
-		git push $remote_u master >> $LOG_FILE 2>&1
+		git push $remote_u master 2>&1
 	done
-done
+done >> $LOG_FILE
 cd $oldWorkingDir
